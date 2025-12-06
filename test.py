cf4575bbae42834f2a4ad3d8212c7d5081914818
v.yunusov <v300187@yandex.ru>
2025-12-07 00:05:20 +0400

    caesar_cipher was added loguru_config was added

--- test.py
+++ test.py
@@ -1,24 +1,48 @@
-from decimal import Decimal
-
-a = Decimal('5')
-b = Decimal('3.1415')
-c = a + b
-c-= Decimal('1')
-
-z = 'Hello'
-z*=10
-
-
-# Для списка чисел вывести сумму чисел до первого отрицательного (не включая его).
-# arr = [3, 5, 7, -2, 6]
-# s = 0
-# for n in arr:
-#     if n < 0:
-#         break
-#     s += n
-# print(s)
-
-
+from loguru_config import setup_logging, logger
+from string import ascii_letters as alpha
 import itertools
-arr = [3, 5, 7, -2, 6]
-print(sum([n for n in itertools.takewhile(lambda x: x >= 0, arr)]))
+
+setup_logging()
+
+def caesar_cipher(data: str, key: int) -> str:
+    result = []
+    alpha_len = len(alpha)
+    remain_key = key % alpha_len
+    for char in data:
+        if char in alpha:
+            pos = alpha.find(char)
+            new_pos = (pos + remain_key) % alpha_len
+            result.append(alpha[new_pos])
+        else:
+            result.append(char)
+    return "".join(result)
+
+
+def caesar_cipher_test():
+    logger.info(caesar_cipher("Doggy", 5))
+    logger.info(caesar_cipher("Python is the BEST!", 20))  # jSNBIH CM NBy VYmn!
+    logger.info(caesar_cipher("abcXYZ", 3))  # Ожидается: defABC
+    logger.info(caesar_cipher("Hello, World!", 13))  # Ожидается: Uryyb, Jbeyq!
+    logger.info(caesar_cipher("123!@#", 10))  # Ожидается: 123!@# (без изменений)
+    logger.info(caesar_cipher("", 5))  # Ожидается: пустая строка
+    # Отрицательное смещение
+    logger.info(caesar_cipher("defDEF", -3))  # Ожидается: abcABC
+
+    # Смещение больше длины алфавита
+    logger.info(caesar_cipher("xyzXYZ", 52))  # Ожидается: xyzXYZ (так как 52 % 52 == 0)
+    logger.info(caesar_cipher("abcABC", 55))  # Ожидается: defDEF (55 % 52 == 3)
+
+    # Смешанные символы
+    logger.info(
+        caesar_cipher("Text with 123 & symbols!", 4)
+    )  # Ожидается: Xibx amxl 123 & wcqfsppw!
+
+def demo_takewhile():
+    arr = [3, 5, 7, -2, 6]
+    logger.info(sum([n for n in itertools.takewhile(lambda x: x >= 0, arr)]))
+
+
+#caesar_cipher_test()
+a = 5
+logger.info(f"a = {a}")
+demo_takewhile()
